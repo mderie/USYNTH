79a4ca6d11c4e09c7dfd383f90a99ccabcfa99aa
mderie <mderie@gmail.com>
2017-07-15 20:55:37 +0200

    Curses runs on Linux

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -12,13 +12,39 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+#ifdef WIN32
+#include <pdcurses.h> //TODO: Check name !
+#else
+#include <curses.h>
+#endif
+
+#include <unistd.h> // for sleep()
+
 using namespace std;
 
 //==============================================================================
 int main (int argc, char* argv[])
 {
+    WINDOW * mainwin;
 
-	cout << "Welcome to USynth Reloaded / The sound explorer" << endl;
+    /*  Initialize ncurses  */
+    if ( (mainwin = initscr()) == NULL ) {
+	fprintf(stderr, "Error initialising ncurses.\n");
+	exit(EXIT_FAILURE);
+    }
 
+    /*  Display "Hello, world!" in the centre of the
+	screen, call refresh() to show our changes, and
+	sleep() for a few seconds to get the full screen effect  */
+    mvaddstr(13, 33, "Hello, world!");
+    refresh();
+    sleep(3);
+
+    /*  Clean up after ourselves  */
+    delwin(mainwin);
+    endwin();
+    refresh();
+
+    cout << "Welcome to USynth Reloaded / The sound explorer" << endl;
     return 0;
 }
---
+++ backup/usynth.lpr.bak
@@ -0,0 +1,21 @@
+program usynth;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, fmain
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
+
